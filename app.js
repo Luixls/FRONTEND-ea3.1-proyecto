e6f73272dd8b3ff22b7531284d414164f2445c54
Luixls <luisaraujo1998@gmail.com>
2024-06-08 10:38:09 -0400

    Modulos/Dependencias y CONFIG

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const app = express();
+
+// Cargar configuración de dotenv desde el archivo datos.env
+dotenv.config({ path: './config/datos.env' });
+
+// Configuración de Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configuración de EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// Configuración del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
