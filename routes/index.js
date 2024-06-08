9e6311748db5b03767330e5d1a86fc60e2c66072
Luixls <luisaraujo1998@gmail.com>
2024-06-08 13:00:39 -0400

    Más cambios de diseño

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get("/", (req, res) => res.render("index"));
 router.get("/about", (req, res) => res.render("about"));
 router.get("/contacto", (req, res) => res.render("contacto"));
 router.post("/contacto", formularioController.guardarFormulario);
+router.get("/comentarios", (req, res) => res.render("comentarios"));
 
 module.exports = router;
