ff17f6c2f8bc062fe49afae7840e1d3a42bcf445
Luixls <luisaraujo1998@gmail.com>
2024-06-08 11:20:27 -0400

    Cambio de Estilos

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const formularioController = require('../controllers/formularioController');
+const formularioController = require("../controllers/formularioController");
 
 // Rutas
-router.get('/', (req, res) => res.render('index'));
-router.get('/about', (req, res) => res.render('about'));
-router.get('/contacto', (req, res) => res.render('contacto'));
-router.post('/contacto', formularioController.guardarFormulario);
+router.get("/", (req, res) => res.render("index"));
+router.get("/about", (req, res) => res.render("about"));
+router.get("/contacto", (req, res) => res.render("contacto"));
+router.post("/contacto", formularioController.guardarFormulario);
 
 module.exports = router;
