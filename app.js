d8ffa1bc169709fdfe30706d56275605d6311e1a
Luixls <luisaraujo1998@gmail.com>
2024-06-08 21:07:48 -0400

    Commit final e informe

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");
 const app = express();
 
 // Cargar configuración de dotenv desde el archivo datos.env
-dotenv.config({ path: './config/datos.env' });
+dotenv.config({ path: "./config/datos.env" });
 
 // Configuración de Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Configuración de EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Rutas
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 // Configuración del servidor
 const PORT = process.env.PORT || 3500;
